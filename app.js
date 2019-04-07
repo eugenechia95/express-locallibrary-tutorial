1a819260850caa08454154e9dfc5d0d996836d30
eugenechia95 <eugenechia95@DESKTOP-OHANHAP.localdomain>
2019-04-07 15:19:45 +0800

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://testuser:testuserpassword@cluster0-dovq2.azure.mongodb.net/test?retryWrites=true';
+var dev_db_url = 'mongodb+srv://testuser:testuserpassword@cluster0-dovq2.azure.mongodb.net/test?retryWrites=true';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
